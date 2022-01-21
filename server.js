add3c97db25943586b64819bffb9ff47e1ecc3fb
rwclutterbuck <rwclutterbuck@outlook.com>
2022-01-21 15:02:19 +0000

    fix require bug in server.js (plus get prototype)

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require('cors');
-const { books } = require(data.js)
+const { books } = require('data.js')
 
 app.use(cors());
 app.use(express.json());
@@ -25,10 +25,14 @@ app.get("/cats", (req, res) => {
 
 app.get("/cats/:name", (req, res) => {
     try {
-        let requestedCatName = req.params.name;
-        let matchingCat = cats.find((cat) => cat.name.toLowerCase() === requestedCatName.toLowerCase());
-        if(!matchingCat) { throw new Error(`We don't have a cat called ${requestedCatName}!`)}
-        res.json(matchingCat)
+        let bookReq = req.params.name;
+        let returnBook = books[bookReq]
+        if (!returnBook) {throw new Error(`Sorry, we don't have information about ${bookReq}.`)}
+        res.json(returnBook)
+        // let requestedCatName = req.params.name;
+        // let matchingCat = cats.find((cat) => cat.name.toLowerCase() === requestedCatName.toLowerCase());
+        // if(!matchingCat) { throw new Error(`We don't have a cat called ${requestedCatName}!`)}
+        // res.json(matchingCat)
     } catch (err) {
         res.status(404).json({ message: err.message })
     }
