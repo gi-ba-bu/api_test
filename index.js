8e44b4079a5d39a01cb9f53eca0e62616907fdcc
giacomo <88339527+gi-ba-bu@users.noreply.github.com>
2022-01-21 22:31:30 +0100

    update

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const app = require('./server.js');
 const port = 3000;
 
-app.listen(port, () => { console.log(`Express now departing from port ${port}`)})
+const app = require('./server.js');
+
+app.listen(port, () => { console.log(`Express now departing from port ${port}`)});
