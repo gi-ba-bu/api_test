0687e769a67565b46cf854da3395ee3265dd6b10
“ewhite1999” <ewhite1999@gmail.com>
2022-01-21 15:43:23 +0000

    formatting

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 const { capitalizeWord } = require("./helpers");
-const cors = require('cors');
-let { books } = require('./data.js')
+const cors = require("cors");
+let { books } = require("./data.js");
 
 app.use(cors());
 app.use(express.json());
@@ -43,9 +43,9 @@ app.get("/cats/:name", (req, res) => {
     if (!returnBook) {
       throw new Error(`Sorry, we don't have information about ${bookReq}.`);
     }
-    } catch(err) {
-        res.status(404).json({ message: err.message })
-    }
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
 });
 
 // U
