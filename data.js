4e59af2358792726bef2a77690e77a307231d66b
“ewhite1999” <ewhite1999@gmail.com>
2022-01-21 14:18:39 +0000

    add book template

---
+++ data.js
@@ -0,0 +1,14 @@
+let bookLib = {
+  gameOfThrones: {
+    title: "Game of Thrones",
+    author: "George R.R. Martin",
+    pages: 780,
+  },
+  aClashOfKings: {
+    title: "A Clash of Kings",
+    author: "George R.R. Martin",
+    pages: 873,
+  },
+};
+
+module.exports(bookLib);
