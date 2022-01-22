fe97464536c256ba2a42d97867d682b757e45560
giacomo <88339527+gi-ba-bu@users.noreply.github.com>
2022-01-22 17:51:58 +0100

    updates

--- server.js
+++ server.js
@@ -1,43 +1,46 @@
-const express = require("express"); /*import express to set up server*/
+const express = require("express"); /*import express to set up the server*/
 const app = express();
 const cors = require("cors"); /*import cors to run server with a middleman*/
 
 
-const { capitalizeWord, capitalizeFirstLetter } = require("./helpers"); /*import helpers*/
-let { books } = require("./data.js"); /*import data*/
+const { capitalizeFirstLetter } = require("./helpers"); /*import helper functions*/
+let { books } = require("./data.js"); /*import book data*/
 
 app.use(cors()); 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Welcome at Node Boooks Admin");
+  res.send("Welcome to Node Boooks Admin");
 });
 
-// C
+// Create
 app.post("/books", (req, res) => {
-  // Get title
-  //const titleVal = req.body.title;
-  //console.log(titleVal);
-  // Provide book object
-  //const newBook = req.body;
-  //console.log(newBook);
-  // Make camel case
-  //const bookKey = capitalizeFirstLetter(titleVal).replace(" ","-");
-  //console.log(bookKey);
-    // Add new book to books Obj
-  //books[bookKey] = newBook;
+  console.log(req.body);
+  //Get title
+  const titleVal = req.body.title;
+  console.log(titleVal);
+  //Provide book object
+  const newBook = req.body;
+  console.log(newBook);
+  //Normalize title (optional)
+  const bookTitle = capitalizeFirstLetter(titleVal);
+  newBook.title = bookTitle;
+  //title string to hamburger case to be used as an object name
+  const bookKeyHamb = bookKey.replaceAll(" ","-").toLowerCase();
+  // Add new book to books Obj
+  books[bookKeyHamb] = newBook;
   // return status and book
-  //res.status(201).json(newBook);
+  res.status(201).json(newBook);
 });
 
-// R
+// Read
 app.get("/books", (req, res) => {
   res.json(books);
 });
 
-app.get("/books/:name", (req, res) => {
+app.get("/books/:title", (req, res) => {
   try {
-    let bookReq = req.params.name;
+    let bookReq = req.params.title;
     let returnBook = books[bookReq];
     if (!returnBook) {
       throw new Error(`Sorry, we don't have information about ${bookReq}.`);
@@ -48,26 +51,33 @@ app.get("/books/:name", (req, res) => {
     }
 });
 
-// U
+// Update
 app.patch('/books/:title', (req, res) => {
     // read new data from body
-    let newData = req.params.title;
-    // find the data to update - go through the list and match name value to value of dynamic route param of :name
-    let requestedBook = req.params.title;
-    let matchingBook = books.find((book) => book.title.toLowerCase() === requestedBook.toLowerCase());
-    
-    // actually update the stored books data
-    let updatedBook = { ...matchingBook, ...newBook }
-    let bookIdx = books.indexOf(matchingBook)
-    books = [ ...books.slice(0, bookIdx), updatedBook, ...books.slice(bookIdx + 1)]
-    
-    res.json(updatedBooks)
-})
+    let newData = req.body;
+    console.log(newData);
+    // change title case to lower case hamburger case 
+    let requestedBookTitle = req.body.title.replaceAll(" ","-").toLowerCase();
+    // find the book to update using title to match 
+    let matchingBook = books[requestedBookTitle];
+    // update with the new information
+    books[requestedBookTitle] = newData;
+    // return all books
+    res.json(books);
+});
 
-// D
-app.delete("/cats", (req, res) => {
-  cats = [];
-  res.status(204).send("Cats gone");
+// Delete
+app.delete("/books", function (req, res) {
+
+try{
+    if (!books === !{}) {
+    throw new Error(`Bad request. All books already deleted.`);
+    }
+    books = {};
+    res.send('Important Message: Got a DELETE request at /books. All books deleted.');
+   } catch(err) {
+    res.status(400).json({ message: err.message }) 
+    }
 });
 
 module.exports = app;
